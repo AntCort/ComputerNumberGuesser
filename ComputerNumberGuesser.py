86c1b06a528467eb4b10759b9ed38c99df3a9abb
AntCort <122321729+AntCort@users.noreply.github.com>
2023-08-23 21:26:43 -0700

    Optimized the code and made it more legible.

--- ComputerNumberGuesser.py
+++ ComputerNumberGuesser.py
@@ -1,44 +1,48 @@
 import random
 
-# Creates the function
-# Range between two numbers is created
-def guess_number():
+class NumberGuesser:
+    def __init__(self, low, high):
+        # Initialize the guesser with a range and a random number within that range
+        self.low = low
+        self.high = high
+        self.guesses = 0
+        self.random_number = random.randint(self.low, self.high)
+
+    def make_guess(self):
+        # Increase the number of guesses made
+        self.guesses += 1
+        # Make a guess and ask the user for feedback
+        print(f"My guess is {self.random_number}. Am I too low, too high, or am I correct?")
+        answer = input("If too low, type 'low'. If too high, type 'high'. If correct, type 'correct': ").lower()
+
+        if answer == "low":
+            # If the guess was too low, adjust the lower bound for the next guess
+            print("I will guess again.")
+            self.low = self.random_number + 1
+        elif answer == "high":
+            # If the guess was too high, adjust the upper bound for the next guess
+            print("I will guess again.")
+            self.high = self.random_number - 1
+        elif answer == "correct":
+            # If the guess was correct, display the result and return True
+            print(f"I guessed it correctly! Your number is {self.random_number}")
+            print(f"It took me {self.guesses} guesses to get it right.")
+            return True
+
+        # Generate a new random number within the adjusted range for the next guess
+        self.random_number = random.randint(self.low, self.high)
+        return False
+
+def main():
+    # Define the range for guessing
     low = 0
     high = 100
-    print(f'Hello, please think of a number between {low} and {high}.')
+    # Create an instance of NumberGuesser
+    guesser = NumberGuesser(low, high)
 
-    # First guess by the computer
-    guesses = 1
-    random_number = random.randint(low, high)
-    print('My first guess is ' + str(random_number) + ". Am I too low, too high, or am I correct?")
-    answer = input('''If too low, type 'low'. If too high, type 'high'. If correct, type 'correct.\n ''').lower()
-    
-    # While the answer isn't 'correct', the computer will
-    # continue to guess
-    while (answer.lower() != 'correct'):
-        
-        # If the answer is too low, the 'low' variable will be updated
-        if (answer == "low"):
-            print("I will guess again.")
-            low = random_number + 1
-            random_number = random.randint(low, high)
-            print('Is ' + str(random_number) + " the number you guessed?")
-            answer = input('''If too low, type 'low'. If too high, type 'high'. If correct, type 'correct.\n ''').lower()
-            guesses = guesses + 1
-            
+    # Keep making guesses until the correct number is guessed
+    while not guesser.make_guess():
+        pass
 
-        # If the answer is too high, the 'high' variable will be updated
-        if (answer == "high"):
-            print("I will guess again.")
-            high = random_number - 1
-            random_number = random.randint(low, high)
-            print('Is ' + str(random_number) + " the number you guessed?")
-            answer = input('''If too low, type 'low'. If too high, type 'high'. If correct, type 'correct.\n ''').lower()
-            guesses = guesses + 1
-            
-
-
-    print(f"I guessed it correctly! Your number is {random_number}")
-    print(f"It took me {guesses} guesses to get it right.")
-
-guess_number()
+if __name__ == "__main__":
+    main()
